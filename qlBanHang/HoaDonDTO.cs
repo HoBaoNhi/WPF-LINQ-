e306b44736ab1562eedd56f1c52190a07605a619
HoBaoNhi <nhi393636@gmail.com>
2025-07-08 17:30:27 +0700

    Add files via upload
    add

---
+++ qlBanHang/HoaDonDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qlBanHang
+{
+    public class HoaDonDTO
+    {
+        public string MaHD { get; set; }
+        public string TenKH { get; set; }
+        public string TenSP { get; set; }
+        public DateTime? NgayLapHD { get; set; }
+        public int? SoLuong { get; set; }
+        public decimal? DonGia { get; set; }
+        public decimal ThanhTien { get; set; }
+    }
+}
